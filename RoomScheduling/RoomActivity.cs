760365dc0f44888f84cab0f2eadab9469d9052a8
PJA10 <alon.w.10@gmail.com>
2019-11-20 15:36:55 +0200

    check

--- RoomScheduling/RoomActivity.cs
+++ RoomScheduling/RoomActivity.cs
@@ -33,7 +33,7 @@ namespace RoomScheduling
         ListView roomUsersLV;
         int roomId;
         ProgressDialog loadingDialog;
-
+        //
 
         async protected override void OnCreate(Bundle savedInstanceState)
         {
